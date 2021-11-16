005b407f9ab7320886ea8d5f6aeafd9b0941c37d
Reilo258 <olivier258@wp.pl>
2021-11-16 18:47:05 +0100

    Funkcja zastępowania i dodawania tekstu. Dodawanie liczb.

--- app/src/main/java/com/example/vanoverveld/MainActivity.kt
+++ app/src/main/java/com/example/vanoverveld/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.vanoverveld
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var liczba1 = 0;
+        var liczba2 = 0;
+        var wynik = 0;
+
+        findViewById<Button>(R.id.zastosujbtn).setOnClickListener {
+            findViewById<TextView>(R.id.wyniktxt).text = findViewById<TextInputEditText>(R.id.wprowadzanie).text;
+        }
+
+        findViewById<Button>(R.id.dodajbtn).setOnClickListener {
+            findViewById<TextView>(R.id.wyniktxt).text = findViewById<TextView>(R.id.wyniktxt).text.toString() + findViewById<TextInputEditText>(R.id.wprowadzanie).text;
+        }
+
+        findViewById<Button>(R.id.dodawaniebtn).setOnClickListener {
+            liczba1 = findViewById<EditText>(R.id.Liczba1Edit).text.toString().toInt();
+            liczba2 = findViewById<EditText>(R.id.Liczba2Edit).text.toString().toInt();
+            wynik = liczba1 + liczba2;
+            findViewById<TextView>(R.id.wyniktxt).text = wynik.toString();
+        }
     }
 }
